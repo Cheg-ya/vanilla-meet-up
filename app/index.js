59d0706d2e6b53ed93d4c79b242f107a34f881da
Yodel-oh-ee-dee <42037023+Cheg-ya@users.noreply.github.com>
2019-01-09 21:34:01 +0900

    build UI, pick up list functionality and code fix for refactor

--- app/index.js
+++ app/index.js
@@ -8,45 +8,91 @@ import 'styles/index.less';
 // You can use jquery for ajax request purpose only.
 import $ from 'jquery';
 
-const scrollUp = document.getElementById('scrollUp');
 const listCover = document.getElementById('listCover');
 const submit = document.getElementById('submit');
 const city = document.getElementById('city');
 const topic = document.getElementById('topic');
 const radius = document.getElementById('radius');
 const loader = document.getElementsByClassName('loading')[0];
-const addItem = document.getElementsByClassName('addfavorite');
-const removeItem = document.getElementsByClassName('removefavorite');
-const favoriteDisplay = document.getElementById('favoriteDisplay');
-const favoriteCover = document.getElementById('favoriteCover');
+const addItem = document.getElementsByClassName('addPickUp');
+const removeItem = document.getElementsByClassName('removePickUp');
 const reloader = document.getElementById('page-reload');
 const form = document.getElementsByClassName('popUp')[0];
 const mapCover = document.getElementsByClassName('mapCover')[0];
+const lookUp = document.getElementById('lookUpButton');
+const mapUp = document.getElementById('mapUpButton');
+const pickUp = document.getElementById('pickUpButton');
 const autocomplete = new google.maps.places.Autocomplete(city);
 let previousCity = '';
+let mapclicked = false;
+let submitted = false;
+let pickUpClicked = false;
+//디자인, 마크 레이블, code convetion, refector
+lookUp.addEventListener('click', function () {
+    let hidden = listCover.classList.contains('hide');
+    let listed = listCover.classList.contains('listMapUp');
+    debugger;
+    if (!hidden && !submitted) {
+        listCover.classList.toggle('hide');
+
+        if (listed) {
+            listCover.classList.remove('listMapUp');
+            mapCover.classList.remove('mapCover-clicked');
+        }
+
+    } else if (hidden && submitted || mapclicked) {
+        listCover.classList.toggle('hide');
+
+        if (listed) {
+            listCover.classList.remove('listMapUp');
+            mapCover.classList.remove('mapCover-clicked');
+        }        
+    }
+});
+
+mapUp.addEventListener('click', function () {
+    let hidden = listCover.classList.contains('hide');
+    let listed = listCover.classList.contains('listLookUp');
+
+    if (!hidden && !mapclicked) {
+        listCover.classList.toggle('hide');
+        
+        if (listed) {
+            listCover.classList.remove('listLookUp');
+            form.classList.remove('popUp-clicked');
+        }
+
+    } else if (hidden && mapclicked) {
+        listCover.classList.toggle('hide');
+
+        if (listed) {
+            listCover.classList.remove('listLookUp');    
+            form.classList.remove('popUp-clicked');
+        }
+    }
+});
+
+pickUp.addEventListener('click', function (e) {
+    pickUpClicked = true;
+    listCover.classList.add('listMapUp');
+    mapCover.classList.add('mapCover-clicked')
+
+    pickUpBox(e.currentTarget.id);
+});
 
 reloader.addEventListener('click', function () {
     location.reload();
 });
 
-scrollUp.addEventListener('click', function () {
-    window.scroll({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
+google.maps.event.addListener(map,'click', function (e) {
+    mapclicked = true;
 
-favoriteDisplay.addEventListener('click', function (e) {
-    favoriteBox(e);
-});
-
-google.maps.event.addListener(map,'click', function (e) { // 지도 클릭
     if (!mapCover.classList.contains('mapCover-clicked')) {
         mapCover.classList.add('mapCover-clicked');
     }
 
     if (topic.value.length === 0 || topic.value === undefined) {
-        topic.value = 'festival';
+        topic.value = 'movie';
     }
 
     if (radius.value.length === 0 || radius.value === undefined) {
@@ -54,7 +100,7 @@ google.maps.event.addListener(map,'click', function (e) { // 지도 클릭
     }
 
     if (city.value.length === 0 || radius.value === undefined) {
-        city.value = 'Seoul, Korea';
+        city.value = 'NY';
     }
 
     const clickedMarker = new google.maps.Marker({
@@ -72,7 +118,9 @@ google.maps.event.addListener(map,'click', function (e) { // 지도 클릭
     });
 });
 
-submit.addEventListener('click', function () { // submit클릭
+submit.addEventListener('click', function () {
+    submitted = true;
+
     if (!form.classList.contains('popUp-clicked')) {
         form.classList.add('popUp-clicked');
     }
@@ -81,7 +129,7 @@ submit.addEventListener('click', function () { // submit클릭
 
     targetCoordinates(geocoder, map);
 });
-//디자인, 마크 레이블, 커버페이지, code convetion, refector
+
 function meetUpAPI (query, radius, location) {
     if (radius % 10 !== 0 || radius === 0) {
         alert('Radius should be 10, 20, 30 miles and so on...');
@@ -139,6 +187,7 @@ function meetUpAPI (query, radius, location) {
                             try {
                                 newData = {
                                     hostName: host.name,
+                                    eventId: list[i].id,
                                     photoLink: host.photo !== undefined ? host.photo.thumb_link : './assets/images/default profile.png',
                                     eventTitle: list[i].name,
                                     groupName: list[i].group.name,
@@ -146,8 +195,8 @@ function meetUpAPI (query, radius, location) {
                                     time: list[i].local_time.slice(0,2) > 12 ? `${(list[i].local_time.slice(0,2) - 12) + list[i].local_time.slice(2)} PM` : `${list[i].local_time} AM`,
                                     rsvpLimit: list[i].rsvp_limit || '00',
                                     rsvpCount: list[i].yes_rsvp_count || 0,
-                                    lng: groupInfo.lon,
-                                    lat: groupInfo.lat
+                                    lng: list[i].venue !== undefined ?  list[i].venue.lon : groupInfo.lon,
+                                    lat: list[i].venue !== undefined ?  list[i].venue.lat : groupInfo.lat
                                 };
                             } catch (e){
                                 loader.classList.add('hide');
@@ -169,42 +218,21 @@ function meetUpAPI (query, radius, location) {
         }).then(function (list) {
             localStorage.setItem('originData', JSON.stringify(list));
 
-            const listTemplate = _.template(
-                `<% _.forEach(list, function (event) { %>
-                    <div class="event">
-                        <div class="eventTitle"><%= event.eventTitle %></div>
-                        <div class="host">
-                            <img class="hostPhoto" src="<%= event.photoLink %>" />
-                            <div class="hostName"><%= event.hostName %></div>
-                        </div>
-                        <div class="detail">
-                            <div class="groupName"><%= event.groupName %></div>
-                            <div class="date"><%= event.date %></div>
-                            <div class="time"><%= event.time %></div>
-                            <div class="rsvp">RSVP <%= event.rsvpCount %>/<%= event.rsvpLimit %></div>
-                        </div>
-                        <div class="addfavorite"><i class="fas fa-map-marked-alt"></i></div>
-                        <div class="removefavorite hide"><i class="far fa-trash-alt"></i></div>
-                    </div>
-                <% }) %>`);
+            listTemplate(list);
+            clearMarker();
+            createMarker(list);
 
-            const eventList = listTemplate({list: list});
+            if (submitted) {
+                listCover.classList.remove('mapLookUp');
+                listCover.classList.add('listLookUp');     
+                submitted = false;
 
-            listCover.innerHTML = eventList;
-            loader.classList.add('hide');
-            // form.classList.remove('hide');
-
-            for (let i = 0; i < listCover.children.length; i++) {
-                (function (idx) {
-                    addItem[i].addEventListener('click', function (e) {
-                        favoriteBox(e, idx);
-                    });
-
-                    removeItem[i].addEventListener('click', function (e) {
-                        favoriteBox(e, idx);
-                    });
-                })(i);
+            } else if (mapclicked) {
+                listCover.classList.remove('listLookUp');
+                listCover.classList.add('listMapUp');
+                mapclicked = false;
             }
+
 
         }).catch(function (err) {
             alert(err);
@@ -213,12 +241,12 @@ function meetUpAPI (query, radius, location) {
 }
 
 function targetCoordinates (geocoder, map) {
-    const address = city.value || 'seoul';
-    const query = topic.value || 'festival';
+    const currentCity = city.value || 'NY';
+    const query = topic.value || 'movie';
     const radiusRange = radius.value || 10;
     let location;
 
-    if (previousCity === address) {
+    if (previousCity === currentCity) {
         meetUpAPI(query, radiusRange, {
             lat: map.getCenter().lat(),
             lng: map.getCenter().lng()
@@ -227,10 +255,10 @@ function targetCoordinates (geocoder, map) {
         return;
 
     } else {
-        previousCity = address;
+        previousCity = currentCity;
     }
 
-    geocoder.geocode({'address': address}, function (results, status) {
+    geocoder.geocode({'address': currentCity}, function (results, status) {
         location = results[0].geometry.location;
 
         if (status == 'OK') {
@@ -251,8 +279,25 @@ function targetCoordinates (geocoder, map) {
             });
 
         } else {
-            alert('English keywords recommended');
+            alert(`Error status: ${status}`);
         }
+    });
+}
+
+function createMarker (list) {
+    list.forEach(function (v, i) {
+        setTimeout(function () {
+            const place = {
+                lat: list[i].lat,
+                lng: list[i].lng
+            };
+
+            markers.push(new google.maps.Marker({
+                position: place,
+                map:map,
+                animation: google.maps.Animation.DROP
+            }));
+        }, i * 200);
     });
 }
 
@@ -260,91 +305,138 @@ function clearMarker() {
     markers.forEach(function(marker) {
         marker.setMap(null);
     });
+
     markers = [];
 }
 
-function favoriteBox (e, idx) {
-    if (e === 'renew' || e.currentTarget.id === 'favoriteDisplay') {
+function pickUpBox (e, idx) {
+    if (e === 'pickUpButton' && !idx) {
         const list = [];
-        const keys = Object.keys(localStorage);
 
-        for (let i = 0; i < localStorage.length; i++) {
-            const oldKey  = keys[i].split(' ');
-            if (oldKey[0] === 'list' && +oldKey[1] !== i) {
-                const temp = JSON.parse(localStorage.getItem(keys[i]));
-                const newKey = `list ${i}`;
+        if (localStorage.originData === undefined || localStorage.length <= 2) {
+            alert('Your pick up list is empty!')
 
-                localStorage.removeItem(keys[i]);
-                localStorage.setItem(newKey, JSON.stringify(temp));
-                list.push(temp);
-            } else if (oldKey[0] === 'list') {
-                list.push(JSON.parse(localStorage.getItem(keys[i])));
+            pickUpClicked = false;
+
+            return;
+        }
+
+        for (let k in localStorage) {
+            let id = k.split(' ')[0];
+            
+            if (id === 'id') {
+                list.push(JSON.parse(localStorage[k]));
             }
         }
 
-        const favoriteTemplate = _.template(
-            `<% _.forEach(list, function (event) { %>
-                <div class="favoriteEvent">
-                    <div class="favoriteEventTitle"><%= event.eventTitle %></div>
-                    <div class="favoriteHost">
-                        <img class="favoriteHostPhoto" src="<%= event.photoLink %>" />
-                        <div class="favoriteHostName"><%= event.hostName %></div>
-                    </div>
-                    <div class="favoriteDetail">
-                        <div class="favoriteGroupName"><%= event.groupName %></div>
-                        <div class="favoriteDate"><%= event.date %></div>
-                        <div class="favoriteTime"><%= event.time %></div>
-                        <div class="favoriteRsvp">RSVP <%= event.rsvpCount %>/<%= event.rsvpLimit %></div>
-                    </div>
-                    <div class="deletefavorite"><i class="fas fa-eraser"></i></div>
-                </div>
-            <% }) %>`);
+        listTemplate(list);
+        clearMarker();
+        createMarker(list);
 
-        const deletefavorite = document.getElementsByClassName('deletefavorite');
-        const favoriteList = favoriteTemplate({list: list});
-
-        favoriteCover.innerHTML = favoriteList;
-        listCover.classList.add('hide');
-        // document.getElementById('searchPage').classList.add('hide');
-
-        for (let i = 0; i < favoriteCover.children.length; i++) {
-            (function (idx) {
-                deletefavorite[i].addEventListener('click', function () {
-                    localStorage.removeItem(`list ${idx}`);
-                    favoriteBox('renew');
-                });
-            })(i);
-        }
-
-        list.forEach(function (v, i) {
-            setTimeout(function () {
-                const place = {
-                    lat: list[i].lat,
-                    lng: list[i].lng
-                };
-
-                markers.push(new google.maps.Marker({
-                    position: place,
-                    map:map,
-                    animation: google.maps.Animation.DROP
-                }));
-            }, i * 200);
-        });
+        pickUpClicked = false;
 
         return;
     }
-    
-    const originList = JSON.parse(localStorage.originData);
-    const list = originList[idx];
 
-    if (e.currentTarget.classList.contains('addfavorite')) {
-        localStorage.setItem(`list ${idx}`, JSON.stringify(list));
-        e.currentTarget.classList.add('hide');
+    
+    let lists = [];
+
+    if (e[0] === 'pickUp') {
+        for (let k in localStorage) {
+            let id = k.split(' ')[0];
+            
+            if (id === 'id') {
+                lists.push(JSON.parse(localStorage[k]));
+            }
+        }
+
+    } else if (e[0] === 'list') {
+       lists = JSON.parse(localStorage.originData);
+    }
+
+    const singleList = lists[idx];    
+    debugger;
+    if (e[1] === 'add') {
+        localStorage.setItem(`id ${singleList.eventId}`, JSON.stringify(singleList));
+        addItem[idx].classList.add('hide');
         removeItem[idx].classList.remove('hide');
         
-    } else {
-        localStorage.removeItem(`list ${idx}`, JSON.stringify(list));
-        e.currentTarget.classList.add('hide');
+    } else if (e[1] === 'remove') {
+        localStorage.removeItem(`id ${singleList.eventId}`, JSON.stringify(singleList));
+        removeItem[idx].classList.add('hide');
         addItem[idx].classList.remove('hide');
+
+    } else {
+        throw new Error('Unvaild input value');
     }
+}
+// 즐찾 분기 애니메이션
+function listTemplate (list) {
+    if (!Array.isArray(list)) {
+        throw new Error('List is not an Array');
+    }
+
+    const listTemplate = _.template(
+        `<% _.forEach(list, function (event) { %>
+            <div class="event">
+                <div class="host">
+                    <img class="hostPhoto" src="<%= event.photoLink %>" />
+                    <span class="hostName"><%= event.hostName %></span>
+                </div>
+                <div class="detail">
+                    <div class="eventTitle"><%= event.eventTitle %></div>
+                    <div class="groupName">Group: <%= event.groupName %></div>
+                    <div class="date">Date: <%= event.time %> <%= event.date %></div>
+                    <div class="rsvp">RSVP: <%= event.rsvpCount %>/<%= event.rsvpLimit %></div>
+                </div>
+                <div class="addPickUp">
+                    <i class="far fa-star"></i>
+                    <span class="tooltip">Click to add</span>
+                </div>
+                <div class="removePickUp hide">
+                    <i class="fas fa-star"></i>
+                    <span class="tooltip">Click to remove</span>
+                </div>
+            </div>
+        <% }) %>`);
+
+    const eventList = listTemplate({list: list});
+
+    listCover.innerHTML = eventList;
+
+    if (pickUpClicked) {
+        for (let i = 0; i < listCover.children.length; i++) {
+            (function (idx) {
+                addItem[i].addEventListener('click', function () {
+                    pickUpBox(['pickUp', 'add'], idx);
+                });
+    
+                removeItem[i].addEventListener('click', function () {
+                    pickUpBox(['pickUp', 'remove'], idx);
+                });
+                
+                addItem[i].classList.add('hide');
+                removeItem[i].classList.remove('hide');
+            })(i);
+        }
+
+        pickUpClicked = false;
+
+    } else {
+        for (let i = 0; i < listCover.children.length; i++) {
+            (function (idx) {
+                addItem[i].addEventListener('click', function (e) {
+                    debugger;
+                    pickUpBox(['list', 'add'], idx);
+                });
+
+                removeItem[i].addEventListener('click', function (e) {
+                    pickUpBox(['list', 'remove'], idx);
+                });
+            })(i);
+        }
+    }
+
+    loader.classList.add('hide');
+    listCover.classList.remove('hide');
 }
